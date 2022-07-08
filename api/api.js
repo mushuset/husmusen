32976e341a7f497d423e2f75fe9a9af048a7bffd
BurnyLlama <burnyllama@protonmail.com>
2022-07-08 10:59:46 +0200

    `DBInfo` now reads from a file.

--- api/api.js
+++ api/api.js
@@ -1,25 +1,19 @@
 import { Router } from "express"
+import fs from "fs/promises"
 import api_1_0_0 from "./1.0.0/api.js"
 
 const api = Router()
 
 api.use("/1.0.0", api_1_0_0)
-api.get("/db_info", (req, res) => res.json({
-    "protocolVersion": "1.0.0",
-    "protocolVersions": [ "1.0.0", "0.8.0" ],
-    "supportedInputFormats": [ "YAML", "JSON" ],
-    "supportedOutputFormats": [ "YAML", "JSON" ],
-    "instanceName": "Husmusen på Museum",
-    "museumDetails": {
-      "name": "Museum",
-      "description": "Ett helt vanligt museum.",
-      "address": "Gatanvägen 4",
-      "location": "Kungshamn",
-      "coordinates": "0°0′0″ N, 25°0′0″ W",
-      "website": "https://example.com"
-    }
-}))
-api.get("/db_info/version", (req, res) => res.send("1.0.0"))
-api.get("/db_info/versions", (req, res) => res.send("1.0.0"))
+api.get("/db_info", (req, res) => {
+    fs.readFile("./config/db_info.json")
+        .then(file   => file.toString())
+        .then(dbInfo => res.contentType("application/json").send(dbInfo))
+        .catch(err   => res.status(500).send(err))
+})
+
+// TODO: These should maybe be read from a file as well?
+api.get("/db_info/version",  (req, res) => res.contentType("text/plain").send("1.0.0"))
+api.get("/db_info/versions", (req, res) => res.contentType("text/plain").send("1.0.0"))
 
 export default api
