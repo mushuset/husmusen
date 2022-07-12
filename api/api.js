9acf3b8c3fe347fa53b67eebcd6c29b99adb14f4
BurnyLlama <burnyllama@protonmail.com>
2022-07-12 11:03:42 +0200

    /api/db_info/* now reads from the same file.

--- api/api.js
+++ api/api.js
@@ -1,19 +1,29 @@
 import { Router } from "express"
-import fs from "fs/promises"
+import getDBInfo from "../lib/DBInfo.js"
 import api_1_0_0 from "./1.0.0/1.0.0.js"
 
 const api = Router()
 
 api.use("/1.0.0", api_1_0_0)
-api.get("/db_info", (req, res) => {
-    fs.readFile("./config/db_info.json")
-        .then(file   => file.toString())
-        .then(dbInfo => res.sendit(dbInfo))
-        .catch(err   => res.status(500).send(err))
-})
+api.get(
+    "/db_info",
+    (req, res) => getDBInfo()
+        .then(DBInfo => res.sendit(DBInfo))
+        .catch(err => res.status(500).send(err))
+)
 
 // TODO: These should maybe be read from a file as well?
-api.get("/db_info/version",  (req, res) => res.contentType("text/plain").send("1.0.0"))
-api.get("/db_info/versions", (req, res) => res.contentType("text/plain").send("1.0.0"))
+api.get(
+    "/db_info/version",
+    (req, res) => getDBInfo()
+        .then(DBInfo => res.header("Content-Type", "text/plain").send(DBInfo.protocolVersion))
+        .catch(err => res.status(500).send(err))
+)
+api.get(
+    "/db_info/versions",
+    (req, res) => getDBInfo()
+        .then(DBInfo => res.header("Content-Type", "text/plain").send(DBInfo.protocolVersions.join(",")))
+        .catch(err => res.status(500).send(err))
+)
 
 export default api
