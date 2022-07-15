653a59850e1438119e46454b0d861386fe951264
BurnyLlama <burnyllama@protonmail.com>
2022-07-15 10:13:03 +0200

    Added rate limit for `app`.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import getLogger from "./lib/log.js"
 import { startDB } from "./lib/database.js"
 import api from "./api/api.js"
 import requestHandler from "./lib/requestHandler.js"
+import rateLimit from "express-rate-limit"
 import njk from "nunjucks"
 import url2njk from "./lib/url2njk.js"
 
@@ -32,9 +33,19 @@ njk.configure(
     }
 )
 
+// The app heavily uses filesystem, so rate limit it.
+// TODO: It isn't too hard to make a proper fix for this and make one route per page, instead of
+// this `url2njk`-thing. It should be done, considering it is *easy*.
+const appRateLimiter = rateLimit({
+    windowMs: 5000,
+    max: 10,
+    standardHeaders: true,
+    legacyHeaders: false
+})
+
 husmusen.use("/api",   api)
-husmusen.get('/app',   url2njk)
-husmusen.get('/app/*', url2njk)
+husmusen.get('/app',   appRateLimiter, url2njk)
+husmusen.get('/app/*', appRateLimiter, url2njk)
 
 husmusen.get("/", (req, res) => res.redirect("/app"))
 husmusen.listen(PORT, () => log("Husmusen started and listening on port:", PORT))
