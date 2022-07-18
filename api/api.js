09d64c3a8c3b9cd8f460547a29af5f9e8e429f26
BurnyLlama <burnyllama@outlook.com>
2022-07-18 11:15:36 +0200

    Ratelimit /api/auth

--- api/api.js
+++ api/api.js
@@ -2,11 +2,18 @@ import { Router } from "express"
 import api_1_0_0 from "./1.0.0/1.0.0.js"
 import authApi from "./auth.js"
 import dbInfoApi from "./db_info.js"
+import rateLimit from "express-rate-limit"
 
 const api = Router()
+const authRateLimit = rateLimit({
+    windowMs: 1000,
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false
+})
 
 api.use("/1.0.0", api_1_0_0)
-api.use("/auth", authApi)
+api.use("/auth", authRateLimit, authApi)
 api.use("/db_info", dbInfoApi)
 
 export default api
