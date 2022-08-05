b86402e6c297ebe756e746c3790636de8eb5206e
BurnyLlama <burnyllama@protonmail.com>
2022-08-05 11:24:14 +0200

    Work on the UI...

---
+++ app/static/scripts/login.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("#login-form")
+
+// This will redirect the user to the control panel if they are already logged in.
+if (new Date(Date.now()) < new Date(localStorage.getItem("api-token-valid-until")))
+    location.assign("/app/control_panel")
+
+// Set up the form to work properly.
+form.addEventListener(
+    "submit",
+    event => {
+        event.preventDefault()
+        const formData = new FormData(form)
+        const username = formData.get("username")
+        const password = formData.get("password")
+        const requestPayload = JSON.stringify({ username, password })
+
+        fetch(
+            "/api/auth/login",
+            {
+                body: requestPayload,
+                method: "POST",
+            }
+        )
+            // Handle the response.
+            .then(
+                response => new Promise(
+                    async (resolve, reject) => {
+                        if (response.status !== 200)
+                            reject(await response.json())
+
+                        resolve(await response.json())
+                    }
+                )
+            )
+            // Handle the response data.
+            .then(
+                data => {
+                    localStorage.setItem("api-token", data.token)
+                    localStorage.setItem("api-token-valid-until", data.validUntil)
+                }
+            )
+            // Catch errors.
+            .catch(
+                err => {
+                    if (err.errorCode === "ERR_INVALID_PASSWORD")
+                        return alert("Fel lösenord!")
+
+                    alert("Error! Kolla i konsolen för mer information.")
+                    console.error(err)
+                }
+            )
+    }
+)
